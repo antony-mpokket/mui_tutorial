ce5e036deb635a62c045481197752d45e9466b95
Antony <antony.mclarensv@mpokket.com>
2022-11-23 19:59:14 +0530

    new muiselect

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Box, TextField, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 export const MuiSelect = () => {
+  const [countries, setCountries] = useState<string[]>([]);
+  console.log({ countries });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+    // setCountries(event.target.value as string);
+    const value = event.target.value
+    setCountries(typeof value === 'string' ? value.split(',') : value)
+  };
   return (
     <div>
       <Typography
@@ -18,10 +27,31 @@ export const MuiSelect = () => {
           style={{ color: "#0018ff" }}
           href="https://mui.com/material-ui/react-select/#main-content"
         >
-          Click here test
+          Click here
         </a>
       </Typography>
       <hr />
+
+      <Box width="250px" sx={{ paddingLeft: "20px" }}>
+        <TextField
+          label="Select Country"
+          select
+          value={countries}
+          onChange={handleChange}
+          fullWidth
+          SelectProps={{
+            multiple: true,
+          }}
+          size='small'
+          color="secondary"
+          helperText="Please select a country"
+          error
+        >
+          <MenuItem value="IN">India</MenuItem>
+          <MenuItem value="US">America</MenuItem>
+          <MenuItem value="AU">Australia</MenuItem>
+        </TextField>
+      </Box>
     </div>
   );
 };
