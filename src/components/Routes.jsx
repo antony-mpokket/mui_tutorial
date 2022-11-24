8fa5306ab64672983044464e687f5855daf21aff
Antony <antony.mclarensv@mpokket.com>
2022-11-24 17:06:12 +0530

    MuiCheckbox

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, Outlet} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import { MuiButton } from "./MuiButton";
 import { MuiTypography } from "./MuiTypography";
 import { MuiTextField } from "./MuiTextField";
 import { MuiSelect } from "./MuiSelect";
-import { MuiRadioButton } from "./MuiRadioButton";
+import {MuiRadioButton} from "./MuiRadioButton";
+import MuiCheckbox from "./MuiCheckbox";
 
 const CustomRoutes = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/typography" element={<MuiTypography />} /> 
-            <Route path="/button" element={<MuiButton />} />
-            <Route path="/textfield" element={<MuiTextField />} />
-            <Route path="/select" element={<MuiSelect />} />
-            <Route path="/radio-button" element={<MuiRadioButton />} />
-        </Routes>
+      <Routes>
+        <Route path="/typography" element={<MuiTypography />} />
+        <Route path="/button" element={<MuiButton />} />
+        <Route path="/textfield" element={<MuiTextField />} />
+        <Route path="/select" element={<MuiSelect />} />
+        <Route path="/radio-button" element={<MuiRadioButton />} />
+        <Route path="/checkbox" element={<MuiCheckbox />} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default CustomRoutes
+export default CustomRoutes;
