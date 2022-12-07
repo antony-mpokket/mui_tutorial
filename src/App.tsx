4aa40b94742c512b72f1bad5388b7042775b1085
Antony <antony.mclarensv@mpokket.com>
2022-12-07 11:54:24 +0530

    MuiPicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
 import './App.css';
 import CustomRoutes from './components/Routes';
+import {LocalizationProvider} from '@mui/lab'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+ 
 
 function App() {
   return (
-    <div className="App">
+    // <LocalizationProvider dateAdapter={AdapterDateFns}>
+    //   <div className="App">
+    //     {/* <MuiTypography /> */}
+    //     <CustomRoutes />
+    //   </div>
+    // </LocalizationProvider>
+    
+      <div className="App">
         {/* <MuiTypography /> */}
-      <CustomRoutes />
-    </div>
+        <CustomRoutes />
+      </div>
   );
 }
 
